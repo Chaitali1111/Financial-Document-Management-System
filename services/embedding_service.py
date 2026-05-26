64710f2191aa60531a4a240ee96c938f4e54b344
Chaitali1111 <166948376+Chaitali1111@users.noreply.github.com>
2026-05-26 13:23:31 +0530

    fixed embedding functions

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -1,2 +1,39 @@
+import chromadb
+
+client = chromadb.Client()
+
+collection = client.create_collection(
+    name="documents"
+)
+
+
 def generate_embedding(text):
+
     return [float(len(text))] * 10
+
+
+def store_embeddings(chunks):
+
+    for i, chunk in enumerate(chunks):
+
+        embedding = generate_embedding(chunk)
+
+        collection.add(
+            documents=[chunk],
+            embeddings=[embedding],
+            ids=[str(i)]
+        )
+
+    return "Embeddings stored successfully"
+
+
+def search_query(query):
+
+    query_embedding = generate_embedding(query)
+
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=2
+    )
+
+    return results
